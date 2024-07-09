9b2322b54434e513c26d3ad496d96721755c70fd
rohit995049 <161129526+rohit995049@users.noreply.github.com>
2024-07-09 06:52:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,30 +41,3 @@ logging.basicConfig(
         logging.StreamHandler(),
     ],
 )
-
-# Auth Users
-AUTH_USERS = [ int(chat) for chat in Config.AUTH_USERS.split(",") if chat != '']
-
-# Prefixes 
-prefixes = ["/", "~", "?", "!"]
-
-plugins = dict(root="plugins")
-if __name__ == "__main__" :
-    bot = Client(
-        "StarkBot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        sleep_threshold=20,
-        plugins=plugins,
-        workers = 50
-    )
-    
-    async def main():
-        await bot.start()
-        bot_info  = await bot.get_me()
-        LOGGER.info(f"<--- @{bot_info.username} Started (c) STARKBOT --->")
-        await idle()
-    
-    asyncio.get_event_loop().run_until_complete(main())
-    LOGGER.info(f"<---Bot Stopped-->")
